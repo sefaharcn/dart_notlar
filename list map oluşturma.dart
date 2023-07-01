afa93716bb40682fa46c74cc59abac56e2b37481
sefaharcn <73999312+sefaharcn@users.noreply.github.com>
2023-07-01 04:16:00 +0300

    Create list map oluşturma.dart

---
+++ list map oluşturma.dart
@@ -0,0 +1,21 @@
+const final kavramı
+const sabit=’bu değişken daha sonra değiştirilece’;
+final sonhaldekiDegisken=’bu değişkenide daha sonradan değiştiremeyiz’;
+Değişken çeşitleri
+kaçış operatörü
+int= 10;
+double= 3.15;
+var= “ali”; var= 65.12;
+String=”bu bir ‘metin’ değişkenidir”;
+String=”Ahmet \’ Ben artık eve gelmeyeceğim\’ dedi”; //ters slashlar burada kaçış operatörüz $ işareti basmak içinde kullanılabailir.
+String=””” bu çok satırlı string oluşturmadır alt satıra geçilebilir 3 tırnak kullanılır”””;
+Print(‘$a $b $c’); bu şekilded değişkenleri toplayabilirsiniz stringleri;
+Bool kapiAcikmi = false // boolen doğru yanlış kavramı.2 seçenek alır.
+Liste oluşturma []
+List sinifListesi= [‘ali’,’veli’,33];
+List<int> numaralar=[33,23,66];
+//Generic yapınca değişkenler generic tipinde olmaz zorundadır.
+List<String> isimler=[‘ali’,’veli’];
+Map oluşturma{}
+var kimlik={2:’ahmet’,3:’mehmet’};
+print(kimlik[2]); // ekrana ahmet basar
