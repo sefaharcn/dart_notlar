510a306b5e08512c4e2cfee82009074078a36462
sefaharcn <73999312+sefaharcn@users.noreply.github.com>
2023-07-01 04:14:04 +0300

    Create class kavramı.dart

---
+++ class kavramı.dart
@@ -0,0 +1,29 @@
+* Inheritance: “kalıtım demek” classleri birbirine bağlama 
+
+Extends : Eklenen Classın tüm özellikleri alır.
+
+abstract Class: içi boş değerler üretir ancak extend edildiğinde oradaki methodlar oluşturulmak zorundadir.
+@override edilmesi gerekir.
+abstract Class madde{ 
+Kutle(); // boş method, Abstract method
+Hacim();
+Bool renklimi(g_deger){return g_deger;}
+// bu method override edilmesine gerek yok içi dolu.
+  
+}
+Class Esya entends madde {
+String marka;
+@override
+ Kutle(){}
+ //TODO implement kutle
+@override
+ Hacim(){}
+//TODO implement hacim
+}
+
+Class Kapi extends Esya{
+String Renk;
+String uretimYılı;}
+*extends ettiğiniz methodların soyut olmayan methodları override etmek zorunda değilsiniz
+*fakat implement ettiğiniz zaman bütün methodları @override etmek zorundasınız.
+
