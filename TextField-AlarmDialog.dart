f2f2ea9f270d44a576ec67b2d9b617063b8a3912
sefaharcn <73999312+sefaharcn@users.noreply.github.com>
2023-07-01 04:28:39 +0300

    Create TextField-AlarmDialog.dart

---
+++ TextField-AlarmDialog.dart
@@ -0,0 +1,11 @@
+TextField
+https://www.yazilimdili.net/flutter-programlama/flutter-widget-class-sinif/flutter-textfield-widget-class/
+bu linkte textFieldla ilgibi detaylı açıklama var
+https://www.yazilimdili.net/flutter-dersleri/
+bu sitete çok detaylı dökümanlar var
+AlertView
+Alert widget lazım olunca bakalım
+Çerez Kullanımı
+Uygulama içine çerez kaydetmek için önce
+shared_preferences 2.1.2
+paketini eklemek lazım
